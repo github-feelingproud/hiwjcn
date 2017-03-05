6529d2c46ea14f620cdd911feeb771c771d2d426
王俊 <hiwjcn@live.com>
2017-03-05 13:52:50 +0800

    删除log4net appender

--- lib/log/RedisLogAppender.cs
+++ lib/log/RedisLogAppender.cs
@@ -1,17 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using StackExchange.Redis;
-using log4net;
+﻿/*using Lib.data;
 using log4net.Appender;
 using log4net.Core;
-using System.Collections;
-using Lib.data;
+using System;
 
 namespace Lib.log
 {
+    public class LogLine
+    {
+        private static readonly DateTime EpochStart = new DateTime(1970, 1, 1);
+
+        public LogLine(LoggingEvent loggingEvent)
+        {
+            if (loggingEvent == null) { throw new Exception("loggingEvent不能为null"); }
+            HostName = loggingEvent.LookupProperty(LoggingEvent.HostNameProperty).ToString();
+            Identity = loggingEvent.Identity;
+            UserName = loggingEvent.UserName;
+            Domain = loggingEvent.Domain;
+            TimeStamp = (long)(loggingEvent.TimeStamp.ToUniversalTime() - EpochStart).TotalMilliseconds;
+            Level = loggingEvent.Level.DisplayName;
+            LoggerName = loggingEvent.LoggerName;
+            Thread = loggingEvent.ThreadName;
+            Message = loggingEvent.RenderedMessage;
+            Throwable = loggingEvent.GetExceptionString();
+            //location
+            Class = loggingEvent.LocationInformation?.ClassName;
+            Method = loggingEvent.LocationInformation?.MethodName;
+            File = loggingEvent.LocationInformation?.FileName;
+            Line = loggingEvent.LocationInformation?.LineNumber;
+        }
+
+        public string HostName { get; set; }
+        public string Identity { get; set; }
+        public string UserName { get; set; }
+        public string Domain { get; set; }
+        public string LoggerName { get; set; }
+        public long TimeStamp { get; set; }
+        public string Level { get; set; }
+        public string Thread { get; set; }
+        public string Message { get; set; }
+        public string Throwable { get; set; }
+        public string Class { get; set; }
+        public string Method { get; set; }
+        public string File { get; set; }
+        public string Line { get; set; }
+    }
     /// <summary>
     /// 使用redis存储日志
     /// https://github.com/lokki/RedisAppender
@@ -28,3 +60,4 @@ namespace Lib.log
         }
     }
 }
+*/
