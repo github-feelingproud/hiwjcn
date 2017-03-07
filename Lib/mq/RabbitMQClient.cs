22ae9415088617ab135743554e901961ac7a87dc
王俊 <hiwjcn@live.com>
2017-03-07 15:36:20 +0800

    mq

---
+++ Lib/mq/RabbitMQClient.cs
@@ -0,0 +1,101 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.mq
+{
+    public class RabbitMQClient : IDisposable
+    {
+        private readonly IConnectionFactory _rabbitMqFactory;
+        private readonly Lazy<IConnection> _rabbitMqConnection;
+
+        private static readonly Lazy<RabbitMQClient> defaultClient = new Lazy<RabbitMQClient>();
+        public static RabbitMQClient DefaultClient => defaultClient.Value;
+
+        #region ctor
+        public RabbitMQClient() : this("rabbitmq") { }
+        public RabbitMQClient(string configurationName) : this(RabbitMQSection.FromSection(configurationName)) { }
+        public RabbitMQClient(RabbitMQSection configuration)
+        {
+            _rabbitMqFactory = new ConnectionFactory
+            {
+                AutomaticRecoveryEnabled = true,
+                UseBackgroundThreadsForIO = true,
+                HostName = configuration.HostName,
+                UserName = configuration.UserName,
+                Password = configuration.Password,
+                VirtualHost = configuration.VirtualHost,
+                ContinuationTimeout = TimeSpan.FromMilliseconds(configuration.ContinuationTimeout),
+                HandshakeContinuationTimeout = TimeSpan.FromMilliseconds(configuration.ContinuationTimeout),
+                RequestedConnectionTimeout = configuration.SocketTimeout,
+                SocketReadTimeout = configuration.SocketTimeout,
+                SocketWriteTimeout = configuration.SocketTimeout,
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(1)
+            };
+
+            _rabbitMqConnection = new Lazy<IConnection>(() => _rabbitMqFactory.CreateConnection());
+        }
+        #endregion
+
+        public IConnection Connection => _rabbitMqConnection.Value;
+
+        #region Dispose
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        bool _disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                //释放托管资源，比如将对象设置为null
+            }
+
+            //释放非托管资源
+            if (_rabbitMqConnection != null && _rabbitMqConnection.IsValueCreated)
+            {
+                //Connection.AutoClose = true;
+                Connection.Close();
+                Connection.Dispose();
+            }
+
+            _disposed = true;
+        }
+
+        //~RabbitMQClient()
+        //{
+        //    Dispose(false);
+        //}
+        #endregion
+
+        /// <summary>创建生产者</summary>
+        /// <param name="exchangeName">交换机名称</param>
+        /// <returns>生产者</returns>
+        public RabbitMQProducer CreateProducer(string exchangeName) => new RabbitMQProducer(Connection.CreateModel(), exchangeName);
+
+        #region CreateConsumer
+        /// <summary>创建Ack消费者</summary>
+        public RabbitMQAckConsumer CreateConsumer() => new RabbitMQAckConsumer(Connection.CreateModel());
+
+        /// <summary>创建Ack消费者</summary>
+        /// <param name="consumerName">消费都名称后缀</param>
+        public RabbitMQAckConsumer CreateConsumer(string consumerName) => new RabbitMQAckConsumer(Connection.CreateModel(), consumerName);
+
+        /// <summary>创建Noack消费者</summary>
+        public RabbitMQNoackConsumer CreateNoackConsumer() => new RabbitMQNoackConsumer(Connection.CreateModel());
+
+        /// <summary>创建Noack消费者</summary>
+        /// <param name="consumerName">消费都名称后缀</param>
+        public RabbitMQNoackConsumer CreateNoackConsumer(string consumerName) => new RabbitMQNoackConsumer(Connection.CreateModel(), consumerName);
+        #endregion
+    }
+}
---
+++ Lib/mq/RabbitMQSection.cs
@@ -0,0 +1,65 @@
+﻿using Lib.helper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.mq
+{
+    /// <summary>
+    /// rabitmq，配置
+    /// </summary>
+    public class RabbitMQSection : ConfigurationSection
+    {
+        public static RabbitMQSection FromSection(string name)
+        {
+            return (RabbitMQSection)ConfigurationManager.GetSection(name);
+        }
+
+        [ConfigurationProperty(nameof(HostName), IsRequired = true)]
+        public string HostName
+        {
+            get { return this[nameof(HostName)].ToString(); }
+        }
+
+        [ConfigurationProperty(nameof(UserName), IsRequired = true)]
+        public string UserName
+        {
+            get { return this[nameof(UserName)].ToString(); }
+        }
+
+        [ConfigurationProperty(nameof(Password), IsRequired = false)]
+        public string Password
+        {
+            get { return this[nameof(Password)].ToString(); }
+        }
+
+        [ConfigurationProperty(nameof(VirtualHost), IsRequired = false)]
+        public string VirtualHost
+        {
+            get { return this[nameof(VirtualHost)].ToString(); }
+        }
+
+        /// <summary>默认3000ms</summary>
+        [ConfigurationProperty(nameof(ContinuationTimeout), IsRequired = false)]
+        public int ContinuationTimeout
+        {
+            get
+            {
+                return ConvertHelper.GetInt(this[nameof(ContinuationTimeout)], 3000);
+            }
+        }
+
+        /// <summary>默认2000ms</summary>
+        [ConfigurationProperty(nameof(SocketTimeout), IsRequired = false)]
+        public int SocketTimeout
+        {
+            get
+            {
+                return ConvertHelper.GetInt(this[nameof(SocketTimeout)], 2000);
+            }
+        }
+    }
+}
