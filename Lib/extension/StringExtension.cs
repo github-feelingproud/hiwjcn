ddc74123b5f4a67bcea585af2b0204e8dc2666e2
王俊 <hiwjcn@live.com>
2017-03-08 13:51:30 +0800

    string extension

---
+++ Lib/extension/StringExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib.helper;
+
+namespace Lib.extension
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// 找到#标签#
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static List<string> FindTags(this string s)
+        {
+            return Com.FindTagsFromStr(s);
+        }
+
+        /// <summary>
+        /// 找到@的人
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static List<string> FindAt(this string s)
+        {
+            return Com.FindAtFromStr(s);
+        }
+    }
+}
