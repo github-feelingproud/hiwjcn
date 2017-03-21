dac9416a7413e07330cddf45df5b235d48734426
王俊 <hiwjcn@live.com>
2017-03-21 12:01:35 +0800

    4

--- Lib/extension/ConvertExtension.cs
+++ Lib/extension/ConvertExtension.cs
@@ -10,16 +10,6 @@ namespace Lib.extension
     public static class ConvertExtension
     {
         /// <summary>
-        /// 字典变url格式(a=1&b=3)
-        /// </summary>
-        /// <param name="dict"></param>
-        /// <returns></returns>
-        public static string ToUrlParam(this IDictionary<string, string> dict)
-        {
-            return Com.DictToUrlParams(dict.ToDictionary(x => x.Key, x => x.Value));
-        }
-
-        /// <summary>
         /// 转换为整型
         /// </summary>
         /// <param name="data">数据</param>
@@ -136,24 +126,7 @@ namespace Lib.extension
         {
             return MapperHelper.GetMappedEntity<T>(data);
         }
-
-        /// <summary>
-        /// 把一个字典加入另一个字典，重复就覆盖
-        /// </summary>
-        /// <typeparam name="K"></typeparam>
-        /// <typeparam name="V"></typeparam>
-        /// <param name="dict"></param>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        public static Dictionary<K, V> AddDict<K, V>(this Dictionary<K, V> dict, Dictionary<K, V> data)
-        {
-            foreach (var kv in data)
-            {
-                dict[kv.Key] = kv.Value;
-            }
-            return dict;
-        }
-
+        
         /// <summary>
         /// NameValueCollection转为字典
         /// </summary>
--- Lib/extension/DictExtension.cs
+++ Lib/extension/DictExtension.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lib.helper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,7 @@ namespace Lib.extension
         /// <param name="key"></param>
         /// <param name="func"></param>
         /// <param name="CheckInstance"></param>
-        /// <param name="_locker"></param>
+        /// <param name="_locker">一定要是引用类型</param>
         /// <returns></returns>
         public static T StoreInstance<T>(this IDictionary<string, T> dict, string key, Func<T> func,
             Func<T, bool> CheckInstance = null, object _locker = null, Action<T> releaseInstance = null)
@@ -46,11 +47,39 @@ namespace Lib.extension
                     if (!dict.ContainsKey(key))
                     {
                         var ins = func();
+                        if (!CheckInstance(ins)) { throw new Exception("新生成的实例没有通过验证"); }
                         dict[key] = ins;
                     }
                 }
             }
             return dict[key];
+        }
+
+        /// <summary>
+        /// 字典变url格式(a=1&b=3)
+        /// </summary>
+        /// <param name="dict"></param>
+        /// <returns></returns>
+        public static string ToUrlParam(this IDictionary<string, string> dict)
+        {
+            return Com.DictToUrlParams(dict.ToDictionary(x => x.Key, x => x.Value));
+        }
+
+        /// <summary>
+        /// 把一个字典加入另一个字典，重复就覆盖
+        /// </summary>
+        /// <typeparam name="K"></typeparam>
+        /// <typeparam name="V"></typeparam>
+        /// <param name="dict"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static Dictionary<K, V> AddDict<K, V>(this Dictionary<K, V> dict, Dictionary<K, V> data)
+        {
+            foreach (var kv in data)
+            {
+                dict[kv.Key] = kv.Value;
+            }
+            return dict;
         }
     }
 }
