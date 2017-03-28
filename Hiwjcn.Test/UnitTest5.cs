5e63abd55654b63b10acaa15f03b0f3f64fc334f
王俊 <hiwjcn@live.com>
2017-03-28 15:18:45 +0800

    enum

---
+++ Hiwjcn.Test/UnitTest5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Lib.extension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hiwjcn.Test
+{
+    public enum UserStatusEnum : int
+    {
+        正常用户 = 0,
+        异常用户 = 8,
+        禁用账户 = 9
+    }
+
+    [TestClass]
+    public class UnitTest5
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var data = Enum.GetNames(typeof(UserStatusEnum));
+
+            var items = UserStatusEnum.异常用户.GetItems<int>();
+        }
+    }
+}
---
+++ Lib/extension/EnumExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Lib.extension
+{
+    public static class EnumExtension
+    {
+        public static Dictionary<string, T> GetItems<T>(this Enum data)
+        {
+            //Enum.GetValues();
+            //Enum.GetNames();
+
+            var fields = data.GetType().GetFields(BindingFlags.Static | BindingFlags.Public);
+            var keys = fields.Select(x => x.GetValue(data)).ToList();
+
+            return fields.ToDictionary(x => x.Name, x => (T)x.GetValue(data));
+        }
+
+        public static Dictionary<string, object> GetItems(this Enum data)
+        {
+            var fields = data.GetType().GetFields(BindingFlags.Static | BindingFlags.Public);
+            var keys = fields.Select(x => x.GetValue(data)).ToList();
+
+            return fields.ToDictionary(x => x.Name, x => x.GetValue(data));
+        }
+    }
+}
