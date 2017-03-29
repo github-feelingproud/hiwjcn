94f3fef7656884f3032fc1c019b17e8882c7bfba
王俊 <hiwjcn@live.com>
2017-03-29 15:18:03 +0800

    0

--- Lib/data/DapperLinq.cs
+++ Lib/data/DapperLinq.cs
@@ -5,11 +5,58 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Lib.data
 {
     public class DapperLinq
     {
+        public virtual Dictionary<string, object> Params { get; set; }
+        public virtual string WhereSql { get; set; }
+        public virtual Expression WhereExpress { get; set; }
+
+        private string BuildWhere(Expression e)
+        {
+            if (e == null) { return null; }
+            switch (e.NodeType)
+            {
+                case ExpressionType.Lambda:
+                    return null;
+
+                case ExpressionType.AndAlso:
+                    return null;
+
+                case ExpressionType.GreaterThan:
+                    return null;
+
+                case ExpressionType.GreaterThanOrEqual:
+                    return null;
+
+                case ExpressionType.LessThan:
+                    return null;
+
+                case ExpressionType.LessThanOrEqual:
+                    return null;
+
+                case ExpressionType.MemberAccess:
+                    return null;
+
+                case ExpressionType.Call:
+                    return null;
+
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        public virtual string ToSql()
+        {
+            if (WhereSql == null)
+            {
+                BuildWhere(this.WhereExpress);
+            }
+            return WhereSql;
+        }
     }
     public static class DapperLinqExtension
     {
@@ -53,7 +100,7 @@ namespace Lib.data
         }
 
         public SqlExpressionCompiler(int existingParameterCount = 0, string parameterNamePrefix = DefaultParameterNamePrefix)
-            : this(DialectProvider.Create(), existingParameterCount, parameterNamePrefix)
+            : this(null, existingParameterCount, parameterNamePrefix)
         { }
 
         public SqlExpressionCompiler(ISqlDialect dialect, int existingParameterCount = 0, string parameterNamePrefix = DefaultParameterNamePrefix)
@@ -427,13 +474,13 @@ namespace Lib.data
             string retVal = p.Name;
 
             // Get Column Name to Use
-            var columnAttribute = p.GetCustomAttributes(typeof(SQLinqColumnAttribute), false).FirstOrDefault() as SQLinqColumnAttribute;
+            var columnAttribute = p.GetCustomAttributes(typeof(ColumnAttribute), false).FirstOrDefault() as ColumnAttribute;
             if (columnAttribute != null)
             {
-                if (!string.IsNullOrEmpty(columnAttribute.Column))
+                if (!string.IsNullOrEmpty(columnAttribute.Name))
                 {
                     // Column name explicitly set, use that instead
-                    retVal = columnAttribute.Column;
+                    retVal = columnAttribute.Name;
                 }
             }
 
@@ -807,159 +854,5 @@ namespace Lib.data
         string ParameterPrefix { get; }
         string ParseTableName(string tableName);
         string ParseColumnName(string columnName);
-    }
-    public class SqlServerDialect : ISqlDialect
-    {
-        const string _Space = " ";
-
-        public object ConvertParameterValue(object value)
-        {
-            return value;
-        }
-
-        public void AssertSkip<T>(SQLinq<T> sqLinq)
-        {
-            if (sqLinq.OrderByExpressions.Count == 0)
-            {
-                throw new NotSupportedException("SQLinq: Skip can only be performed if OrderBy is specified.");
-            }
-        }
-
-        const string _parameterPrefix = "@";
-        public string ParameterPrefix
-        {
-            get { return _parameterPrefix; }
-        }
-
-        public string ParseTableName(string tableName)
-        {
-            if (!tableName.StartsWith("["))
-            {
-                return string.Format("[{0}]", tableName);
-            }
-
-            return tableName;
-        }
-
-        public string ParseColumnName(string columnName)
-        {
-            if (!columnName.StartsWith("[") && !columnName.Contains("."))
-            {
-                return string.Format("[{0}]", columnName);
-            }
-
-            return columnName;
-        }
-
-        public string ToQuery(SQLinqSelectResult selectResult)
-        {
-            var orderby = DialectProvider.ConcatFieldArray(selectResult.OrderBy);
-
-            var groupby = DialectProvider.ConcatFieldArray(selectResult.GroupBy);
-
-
-            var sb = new StringBuilder();
-
-            if (selectResult.Distinct == true)
-            {
-                sb.Append("DISTINCT ");
-            }
-
-            // SELECT
-            sb.Append(DialectProvider.ConcatFieldArray(selectResult.Select));
-
-
-            if (selectResult.Skip != null)
-            {
-                if (sb.Length > 0)
-                {
-                    sb.Append(",");
-                }
-                sb.Append(string.Format(" ROW_NUMBER() OVER (ORDER BY {0}) AS [SQLinq_row_number]", orderby));
-            }
-
-            sb.Append(" FROM ");
-
-            if (selectResult.Distinct == true && selectResult.Skip != null && selectResult.Take != null)
-            {
-                sb.Append("(SELECT DISTINCT ");
-                sb.Append(DialectProvider.ConcatFieldArray(selectResult.Select));
-                sb.Append(" FROM ");
-                sb.Append(selectResult.Table);
-                sb.Append(") AS d");
-            }
-            else
-            {
-                sb.Append(selectResult.Table);
-            }
-
-            if (selectResult.Join != null)
-            {
-                foreach (var j in selectResult.Join)
-                {
-                    sb.Append(_Space);
-                    sb.Append(j);
-                }
-            }
-
-            if (!string.IsNullOrEmpty(selectResult.Where))
-            {
-                sb.Append(" WHERE ");
-                sb.Append(selectResult.Where);
-            }
-
-            if (!string.IsNullOrEmpty(groupby))
-            {
-                sb.Append(" GROUP BY ");
-                sb.Append(groupby);
-            }
-
-            if (!string.IsNullOrEmpty(selectResult.Having))
-            {
-                sb.Append(" HAVING ");
-                sb.Append(selectResult.Having);
-            }
-
-
-            var sqlOrderBy = string.Empty;
-            if (orderby.Length > 0)
-            {
-                sqlOrderBy = " ORDER BY " + orderby;
-            }
-
-            if (selectResult.Skip != null)
-            {
-                // paging support
-                var start = (selectResult.Skip + 1).ToString();
-                var end = (selectResult.Skip + (selectResult.Take ?? 0)).ToString();
-                if (selectResult.Take == null)
-                {
-                    if (selectResult.Distinct == true)
-                    {
-                        return string.Format(@"WITH SQLinq_data_set AS (SELECT {0}) SELECT * FROM SQLinq_data_set WHERE [SQLinq_row_number] >= {1} ORDER BY [SQLinq_row_number]", sb.ToString(), start);
-                    }
-                    else
-                    {
-                        return string.Format(@"WITH SQLinq_data_set AS (SELECT {0}) SELECT * FROM SQLinq_data_set WHERE [SQLinq_row_number] >= {1}", sb.ToString(), start);
-                    }
-                }
-
-                return string.Format(@"WITH SQLinq_data_set AS (SELECT {0}) SELECT * FROM SQLinq_data_set WHERE [SQLinq_row_number] BETWEEN {1} AND {2}", sb.ToString(), start, end);
-            }
-            else if (selectResult.Take != null)
-            {
-                var sbQuery = sb.ToString();
-                if (sbQuery.ToLower().StartsWith("distinct "))
-                {
-                    return "SELECT DISTINCT TOP " + selectResult.Take.ToString() + _Space + sbQuery.Substring(9) + sqlOrderBy;
-                }
-                else
-                {
-                    return "SELECT TOP " + selectResult.Take.ToString() + _Space + sbQuery + sqlOrderBy;
-                }
-            }
-
-            return "SELECT " + sb.ToString() + sqlOrderBy;
-        }
     }
 }
