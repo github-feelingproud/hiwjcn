f663f3e6cf82cbfa74ea480014ed87dc18b1a8ef
王俊 <hiwjcn@live.com>
2017-04-01 17:45:51 +0800

    8

---
+++ Hiwjcn.Test/UnitTest6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lib.data;
+using System.Linq.Expressions;
+using Model.User;
+using Lib.extension;
+
+namespace Hiwjcn.Test
+{
+    [TestClass]
+    public class UnitTest6
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            try
+            {
+                Expression<Func<UserModel, bool>> ex = x => x.NickName == "wj";
+
+                var sql = ExpressionToSql.Convert(ex);
+            }
+            catch (Exception e)
+            {
+                e.AddErrorLog();
+            }
+        }
+    }
+}
