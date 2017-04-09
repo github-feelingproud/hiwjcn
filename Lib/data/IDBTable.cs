8a02f96bf4e53617a3fe94ef14d77f652f492f2b
王俊 <hiwjcn@live.com>
2017-04-09 22:04:16 +0800

    3

--- Lib/data/IDBTable.cs
+++ Lib/data/IDBTable.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Lib.extension;
 
 namespace Lib.data
 {
@@ -16,12 +13,86 @@ namespace Lib.data
         //
     }
 
+    /// <summary>
+    /// 约定了数据表默认属性
+    /// </summary>
     public abstract class DBTable : IDBTable
     {
+        /// <summary>
+        /// 自增的主键
+        /// </summary>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public virtual long IID { get; set; }
+
+        /// <summary>
+        /// UUID
+        /// </summary>
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "UID必须是长度为20~100的唯一字符串")]
         public virtual string UID { get; set; }
+        
+        /// <summary>
+        /// 转换为json
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return this.ToJson();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as DBTable);
+        }
+
+        private static bool IsTransient(DBTable obj)
+        {
+            return obj != null && Equals(obj.IID, default(long));
+        }
+
+        private Type GetUnproxiedType()
+        {
+            return GetType();
+        }
+
+        public virtual bool Equals(DBTable other)
+        {
+            if (other == null)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (!IsTransient(this) &&
+                !IsTransient(other) &&
+                Equals(IID, other.IID))
+            {
+                var otherType = other.GetUnproxiedType();
+                var thisType = GetUnproxiedType();
+                return thisType.IsAssignableFrom(otherType) ||
+                        otherType.IsAssignableFrom(thisType);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            if (Equals(IID, default(int)))
+                return base.GetHashCode();
+            return IID.GetHashCode();
+        }
+
+        public static bool operator ==(DBTable x, DBTable y)
+        {
+            return Equals(x, y);
+        }
+
+        public static bool operator !=(DBTable x, DBTable y)
+        {
+            return !(x == y);
+        }
+
     }
 
 }
