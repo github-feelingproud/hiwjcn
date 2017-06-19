63ab78fe3df83085151fcf07fece072d1bf95440
王俊 <hiwjcn@live.com>
2017-06-19 17:39:24 +0800

    8

--- Lib/extension/AdoExtension.cs
+++ Lib/extension/AdoExtension.cs
@@ -22,6 +22,74 @@ using Lib.data;
 namespace Lib.extension
 {
     /// <summary>
+    /// 针对不同数据库的sql方言
+    /// </summary>
+    public interface ISqlDialect
+    { }
+
+    public class MySqlDialect : ISqlDialect
+    { }
+
+    public class SqlServerDialect : ISqlDialect
+    { }
+
+    public class PostgreSqlDialect : ISqlDialect
+    { }
+
+    public static class SimpleOrmExtension
+    {
+        /// <summary>
+        /// 这个字段是数据库自动生成
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <returns></returns>
+        public static bool IsGeneratedInDatabase(this PropertyInfo prop)
+        {
+            return prop.GetCustomAttributes<DatabaseGeneratedAttribute>().Where(m => m.DatabaseGeneratedOption != DatabaseGeneratedOption.None).Any();
+        }
+
+        /// <summary>
+        /// 获取表名
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static string GetTableName(this Type t)
+        {
+            return t.GetCustomAttribute<TableAttribute>()?.Name ?? t.Name;
+        }
+
+        /// <summary>
+        /// 获取字段对应的属性
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static IEnumerable<PropertyInfo> GetColumnsProperties(this Type t)
+        {
+            return t.GetProperties().Where(x => x.CanRead && x.CanWrite).Where(x => !x.GetCustomAttributes<NotMappedAttribute>().Any());
+        }
+
+        /// <summary>
+        /// 获取字段属性的信息
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static (string column, string placeholder) GetColumnInfo(this PropertyInfo p)
+        {
+            var column = p.GetCustomAttribute<ColumnAttribute>()?.Name ?? p.Name;
+            var placeholder = p.Name;
+            return (column, placeholder);
+        }
+
+        /// <summary>
+        /// 是否是主键
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static bool IsPrimaryKey(this PropertyInfo p) => p.GetCustomAttributes<KeyAttribute>().Any();
+
+    }
+
+    /// <summary>
     /// 对dapper的扩展
     /// </summary>
     public static class DapperExtension
@@ -84,6 +152,21 @@ namespace Lib.extension
         }
 
         /// <summary>
+        /// 获取dapper的动态参数
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static DynamicParameters ToDapperParams(this Dictionary<string, object> data)
+        {
+            var args = new DynamicParameters(new { });
+            foreach (var row in data.AsTupleEnumerable())
+            {
+                args.Add(row.key, row.value);
+            }
+            return args;
+        }
+
+        /// <summary>
         /// 获取表结构
         /// </summary>
         /// <typeparam name="T"></typeparam>
@@ -93,21 +176,27 @@ namespace Lib.extension
             Dictionary<string, string> auto_generated_columns, Dictionary<string, string> columns)
             GetTableStructure<T>(this T model) where T : IDBTable
         {
-            var t = model.GetType();
-            var pps = t.GetProperties().Where(x => x.CanRead && x.CanWrite).Where(x => !x.GetCustomAttributes<NotMappedAttribute>().Any());
+            return model.GetType().GetTableStructure();
+        }
+
+        /// <summary>
+        /// 获取表结构
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static (string table_name, Dictionary<string, string> keys,
+            Dictionary<string, string> auto_generated_columns, Dictionary<string, string> columns)
+            GetTableStructure(this Type t)
+        {
+            var pps = t.GetColumnsProperties();
             //表名
-            var table_name = t.GetCustomAttribute<TableAttribute>()?.Name ?? t.Name;
+            var table_name = t.GetTableName();
 
             //读取字段名和sql的placeholder
-            (string column, string placeholder) GetColumn(PropertyInfo p)
-            {
-                var column = p.GetCustomAttribute<ColumnAttribute>()?.Name ?? p.Name;
-                var placeholder = p.Name;
-                return (column, placeholder);
-            }
+            (string column, string placeholder) GetColumn(PropertyInfo p) => p.GetColumnInfo();
 
             //主键
-            var key_props = pps.Where(x => x.GetCustomAttributes<KeyAttribute>().Any()).ToList();
+            var key_props = pps.Where(x => x.IsPrimaryKey()).ToList();
             if (!ValidateHelper.IsPlumpList(key_props))
             {
                 throw new Exception("Model没有设置主键");
@@ -115,11 +204,7 @@ namespace Lib.extension
             var keys = key_props.Select(x => GetColumn(x)).ToDictionary(x => x.column, x => x.placeholder);
 
             //自动生成的字段
-            bool IsGenerated(PropertyInfo x)
-            {
-                return x.GetCustomAttributes<DatabaseGeneratedAttribute>().Where(m => m.DatabaseGeneratedOption != DatabaseGeneratedOption.None).Any();
-            }
-            var auto_generated_props = pps.Where(x => IsGenerated(x)).ToList();
+            var auto_generated_props = pps.Where(x => x.IsGeneratedInDatabase()).ToList();
             var auto_generated_columns = auto_generated_props.Select(x => GetColumn(x)).ToDictionary(x => x.column, x => x.placeholder);
 
             //普通字段
@@ -130,7 +215,6 @@ namespace Lib.extension
                 throw new Exception("无法提取到有效字段");
             }
             var columns = column_props.Select(x => GetColumn(x)).ToDictionary(x => x.column, x => x.placeholder);
-
 
             return (table_name, keys, auto_generated_columns, columns);
         }
@@ -253,6 +337,48 @@ namespace Lib.extension
             {
                 throw new Exception($"无法执行SQL:{sql}", e);
             }
+        }
+
+        [Obsolete("实现比较垃圾")]
+        public static async Task<T> FindByPrimaryKeyAsync<T>(this IDbConnection con, object[] keys,
+            IDbTransaction transaction = null, int? commandTimeout = default(int?))
+        {
+            var structure = typeof(T).GetTableStructure();
+            if (keys.Length != structure.keys.Count) { throw new Exception("传入主键数量和数据表不一致"); }
+            var where = " AND ".Join(structure.keys.Select(x => $"{x.Key}=@{x.Value}"));
+
+            var sql = $"SELECT * FROM {structure.table_name} WHERE {where}";
+
+            var param_dict = new Dictionary<string, object>();
+            var index = 0;
+            foreach (var row in structure.keys.AsTupleEnumerable())
+            {
+                param_dict[row.value] = keys[index++];
+            }
+
+            return (await con.QueryAsync<T>(sql, param_dict.ToDapperParams(),
+                transaction: transaction, commandTimeout: commandTimeout)).FirstOrDefault();
+        }
+
+        [Obsolete("实现比较垃圾")]
+        public static T FindByPrimaryKey<T>(this IDbConnection con, object[] keys,
+            IDbTransaction transaction = null, int? commandTimeout = default(int?))
+        {
+            var structure = typeof(T).GetTableStructure();
+            if (keys.Length != structure.keys.Count) { throw new Exception("传入主键数量和数据表不一致"); }
+            var where = " AND ".Join(structure.keys.Select(x => $"{x.Key}=@{x.Value}"));
+
+            var sql = $"SELECT * FROM {structure.table_name} WHERE {where}";
+
+            var param_dict = new Dictionary<string, object>();
+            var index = 0;
+            foreach (var row in structure.keys.AsTupleEnumerable())
+            {
+                param_dict[row.value] = keys[index++];
+            }
+
+            return con.Query<T>(sql, param_dict.ToDapperParams(),
+                transaction: transaction, commandTimeout: commandTimeout).FirstOrDefault();
         }
     }
 
