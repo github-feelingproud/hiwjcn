049d0c84ca102b79f9795d17c93619dae6215182
王俊 <hiwjcn@live.com>
2017-06-23 23:42:42 +0800

    8

--- Hiwjcn.Core/Domain/Auth/AuthClients.cs
+++ Hiwjcn.Core/Domain/Auth/AuthClients.cs
@@ -12,7 +12,7 @@ namespace Hiwjcn.Core.Domain.Auth
     /// <summary>
     /// auth客户端
     /// </summary>
-    [Table(nameof(AuthClient))]
+    [Table("auth_client")]
     public class AuthClient : IDBTable
     {
         [Key]
@@ -21,19 +21,26 @@ namespace Hiwjcn.Core.Domain.Auth
         public virtual int IID { get; set; }
 
         [Index]
-        [Required(ErrorMessage = "客户端UID必填")]
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "客户端UID必填")]
         public virtual string UID { get; set; }
 
-        [Required(ErrorMessage = "客户端名称必填")]
+        [Required]
+        [StringLength(20, MinimumLength = 1, ErrorMessage = "客户端名称必填")]
         public virtual string ClientName { get; set; }
 
-        [Required(ErrorMessage = "客户端URL必填")]
+        [Required]
+        [Url(ErrorMessage = "客户端地址必须是URL")]
+        [StringLength(1000, ErrorMessage = "客户端地址长度过长")]
         public virtual string ClientUrl { get; set; }
 
+        [Required]
         [Url(ErrorMessage = "客户端图标必填")]
+        [StringLength(1000, ErrorMessage = "客户端地址长度过长")]
         public virtual string LogoUrl { get; set; }
 
-        [Required(ErrorMessage = "客户端用户UID必填")]
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "客户端用户UID必填")]
         public virtual string UserUID { get; set; }
 
         public virtual int IsRemove { get; set; }
@@ -43,6 +50,7 @@ namespace Hiwjcn.Core.Domain.Auth
         public virtual DateTime? UpdateTime { get; set; }
     }
 
+    [Table("auth_scope")]
     public class AuthScope : IDBTable
     {
         [Key]
@@ -50,23 +58,29 @@ namespace Hiwjcn.Core.Domain.Auth
         public virtual int IID { get; set; }
 
         [Index]
-        [Required(ErrorMessage = "scope UID必填")]
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "scope UID必填")]
         public virtual string UID { get; set; }
 
-        [Required(ErrorMessage = "scope 名称必填")]
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "scope 名称必填")]
         public virtual string Name { get; set; }
 
-        [Required(ErrorMessage = "scope 展示名称必填")]
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "scope 展示名称必填")]
         public virtual string DisplayName { get; set; }
 
+        [StringLength(100, ErrorMessage = "描述太长")]
         public virtual string Description { get; set; }
 
         public virtual int Important { get; set; }
 
         public virtual int Sort { get; set; }
 
+        [StringLength(1000, ErrorMessage = "图片地址长度过长")]
         public virtual string ImageUrl { get; set; }
 
+        [StringLength(30, ErrorMessage = "字体图标数据过长")]
         public virtual string FontIcon { get; set; }
 
         public virtual DateTime CreateTime { get; set; }
@@ -74,6 +88,7 @@ namespace Hiwjcn.Core.Domain.Auth
         public virtual DateTime? UpdateTime { get; set; }
     }
 
+    [Table("auth_token")]
     public class AuthToken : IDBTable
     {
         [Key]
@@ -81,23 +96,35 @@ namespace Hiwjcn.Core.Domain.Auth
         public virtual int IID { get; set; }
 
         [Index]
-        [Required(ErrorMessage = "token UID必填")]
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "token UID必填")]
         public virtual string UID { get; set; }
 
-        [Required(ErrorMessage = "token 必填")]
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "token 必填")]
         public virtual string Token { get; set; }
 
-        [Required(ErrorMessage = "refresh token 必填")]
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "refresh token 必填")]
         public virtual string RefreshToken { get; set; }
 
-        [Required(ErrorMessage = "客户端UID 必填")]
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "客户端UID 必填")]
         public virtual string ClientUID { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "用户UID 必填")]
+        public virtual string UserUID { get; set; }
 
         public virtual DateTime ExpiryTime { get; set; }
 
         public virtual DateTime CreateTime { get; set; }
+
+        [NotMapped]
+        public virtual List<AuthScope> Scopes { get; set; }
     }
 
+    [Table("auth_token_scope")]
     public class AuthTokenScope : IDBTable
     {
         [Key]
@@ -105,13 +132,16 @@ namespace Hiwjcn.Core.Domain.Auth
         public virtual int IID { get; set; }
 
         [Index]
-        [Required(ErrorMessage = "auth token UID必填")]
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "auth token UID必填")]
         public virtual string UID { get; set; }
 
-        [Required(ErrorMessage = "token UID 必填")]
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "token UID 必填")]
         public virtual string TokenUID { get; set; }
 
-        [Required(ErrorMessage = "scope UID 必填")]
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "scope UID 必填")]
         public virtual string ScopeUID { get; set; }
 
         public virtual DateTime CreateTime { get; set; }
