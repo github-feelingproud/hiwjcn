5427169afb0dca1c9bdae5008df28cc3be45295f
王俊 <hiwjcn@live.com>
2017-07-19 15:33:02 +0800

    3

---
+++ Hiwjcn.Web/Controllers/AuthManageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lib.mvc;
+using Lib.mvc.user;
+using Lib.helper;
+using Lib.core;
+using Lib.extension;
+using Lib.net;
+using System.Threading.Tasks;
+using Hiwjcn.Core.Infrastructure.Auth;
+
+namespace Hiwjcn.Web.Controllers
+{
+    public class AuthManageController : BaseController
+    {
+        public async Task<ActionResult> Scopes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ActionResult> Clients()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
