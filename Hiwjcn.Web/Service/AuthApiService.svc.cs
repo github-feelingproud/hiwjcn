d38192c6b8c0fdf718099074511a8fe6206a9035
王俊 <hiwjcn@live.com>
2017-09-04 11:11:19 +0800

    9

--- Hiwjcn.Web/Service/AuthApiService.svc.cs
+++ Hiwjcn.Web/Service/AuthApiService.svc.cs
@@ -16,24 +16,27 @@ namespace Hiwjcn.Web.Service
     // 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 AuthApiService.svc 或 AuthApiService.svc.cs，然后开始调试。
     public class AuthApiService : IAuthApiService
     {
+        private async Task<T> X<T>(Func<IAuthApi, Task<T>> func) =>
+            await AppContext.ScopeAsync(async s => await func.Invoke(s.Resolve_<IAuthApi>()));
+
         public async Task<_<TokenModel>> GetAccessToken(string client_id, string client_secret, string code, string grant_type)
         {
-            return await AppContext.ScopeAsync(async s => await s.Resolve_<IAuthApi>().GetAccessTokenAsync(client_id, client_secret, code, grant_type));
+            return await this.X(async s => await s.GetAccessTokenAsync(client_id, client_secret, code, grant_type));
         }
 
         public async Task<_<string>> GetAuthCodeByOneTimeCode(string client_id, string scopeJson, string phone, string sms)
         {
-            return await AppContext.ScopeAsync(async s => await s.Resolve_<IAuthApi>().GetAuthCodeByOneTimeCodeAsync(client_id, scopeJson, phone, sms));
+            return await this.X(async s => await s.GetAuthCodeByOneTimeCodeAsync(client_id, scopeJson, phone, sms));
         }
 
         public async Task<_<string>> GetAuthCodeByPassword(string client_id, string scopeJson, string username, string password)
         {
-            return await AppContext.ScopeAsync(async s => await s.Resolve_<IAuthApi>().GetAuthCodeByPasswordAsync(client_id, scopeJson, username, password));
+            return await this.X(async s => await s.GetAuthCodeByPasswordAsync(client_id, scopeJson, username, password));
         }
 
         public async Task<_<LoginUserInfo>> GetLoginUserInfoByToken(string client_id, string access_token)
         {
-            return await AppContext.ScopeAsync(async s => await s.Resolve_<IAuthApi>().GetLoginUserInfoByTokenAsync(client_id, access_token));
+            return await this.X(async s => await s.GetLoginUserInfoByTokenAsync(client_id, access_token));
         }
     }
 }
