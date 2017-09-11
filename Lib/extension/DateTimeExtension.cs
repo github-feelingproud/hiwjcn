0aac4599cca48a14e9ab861e841a4975d34666f5
王俊 <hiwjcn@live.com>
2017-09-11 09:39:28 +0800

    7

--- Lib/extension/DateTimeExtension.cs
+++ Lib/extension/DateTimeExtension.cs
@@ -60,8 +60,6 @@ namespace Lib.extension
         /// <summary>
         /// 获取格式化字符串，带时分秒，格式："yyyy-MM-dd HH:mm:ss"
         /// </summary>
-        /// <param name="dateTime">日期</param>
-        /// <param name="isRemoveSecond">是否移除秒</param>
         public static string ToDateTimeString(this DateTime dateTime, bool isRemoveSecond = false)
         {
             if (isRemoveSecond)
@@ -72,7 +70,6 @@ namespace Lib.extension
         /// <summary>
         /// 获取格式化字符串，不带时分秒，格式："yyyy-MM-dd"
         /// </summary>
-        /// <param name="dateTime">日期</param>
         public static string ToDateString(this DateTime dateTime)
         {
             return dateTime.ToString("yyyy-MM-dd");
@@ -81,7 +78,6 @@ namespace Lib.extension
         /// <summary>
         /// 获取格式化字符串，不带年月日，格式："HH:mm:ss"
         /// </summary>
-        /// <param name="dateTime">日期</param>
         public static string ToTimeString(this DateTime dateTime)
         {
             return dateTime.ToString("HH:mm:ss");
@@ -90,7 +86,6 @@ namespace Lib.extension
         /// <summary>
         /// 获取格式化字符串，带毫秒，格式："yyyy-MM-dd HH:mm:ss.fff"
         /// </summary>
-        /// <param name="dateTime">日期</param>
         public static string ToMillisecondString(this DateTime dateTime)
         {
             return dateTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
@@ -99,7 +94,6 @@ namespace Lib.extension
         /// <summary>
         /// 获取格式化字符串，不带时分秒，格式："yyyy年MM月dd日"
         /// </summary>
-        /// <param name="dateTime">日期</param>
         public static string ToChineseDateString(this DateTime dateTime)
         {
             return string.Format("{0}年{1}月{2}日", dateTime.Year, dateTime.Month, dateTime.Day);
@@ -108,8 +102,6 @@ namespace Lib.extension
         /// <summary>
         /// 转为n秒前，n分钟前 etc
         /// </summary>
-        /// <param name="dateTime"></param>
-        /// <returns></returns>
         public static string ToFriendlyDateTime(this DateTime dateTime)
         {
             return DateTimeHelper.GetFriendlyDateTime(dateTime);
@@ -118,18 +110,32 @@ namespace Lib.extension
         /// <summary>
         /// 转为n秒前，n分钟前 etc
         /// </summary>
-        /// <param name="dateTime"></param>
-        /// <returns></returns>
         public static string GetSimpleFriendlyDateTime(this DateTime dateTime)
         {
             return DateTimeHelper.GetSimpleFriendlyDateTime(dateTime);
         }
 
         /// <summary>
+        /// 获取当前周的开始结束
+        /// </summary>
+        public static (DateTime start, DateTime end) GetWeekBorder(this DateTime time,
+            DayOfWeek weekStart = DayOfWeek.Monday)
+        {
+            var date = time.Date;
+            while (true)
+            {
+                if (date.DayOfWeek == weekStart)
+                {
+                    break;
+                }
+                date = date.AddDays(-1);
+            }
+            return (date, date.AddDays(7).AddDays(1));
+        }
+
+        /// <summary>
         /// 获取一天的开始和第二天的开始
         /// </summary>
-        /// <param name="dateTime"></param>
-        /// <returns></returns>
         public static (DateTime start, DateTime end) GetDateBorder(this DateTime dateTime)
         {
             var date = dateTime.Date;
@@ -139,9 +145,6 @@ namespace Lib.extension
         /// <summary>
         /// 判断是否是同一天
         /// </summary>
-        /// <param name="dateTime"></param>
-        /// <param name="time"></param>
-        /// <returns></returns>
         public static bool IsSameDay(this DateTime dateTime, DateTime time)
         {
             var border = dateTime.GetDateBorder();
@@ -151,8 +154,6 @@ namespace Lib.extension
         /// <summary>
         /// 是今天
         /// </summary>
-        /// <param name="time"></param>
-        /// <returns></returns>
         public static bool IsToday(this DateTime time)
         {
             return time.IsSameDay(DateTime.Now);
@@ -161,8 +162,6 @@ namespace Lib.extension
         /// <summary>
         /// 是昨天
         /// </summary>
-        /// <param name="time"></param>
-        /// <returns></returns>
         public static bool IsYesterday(this DateTime time)
         {
             return time.IsSameDay(DateTime.Now.AddDays(-1));
