85def1fd7d3c050062f6ca9110a80637897519ab
王俊 <hiwjcn@live.com>
2017-09-22 18:22:36 +0800

    9

---
+++ Hiwjcn.Core/EnumsBundle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiwjcn.Core
+{
+    public enum OrderStatusEnum : int
+    {
+        待付款 = 1,
+        待发货 = 2,
+        待收货 = 3,
+        待评价 = 4,
+        关闭 = 8,
+        取消 = 9
+    }
+}
