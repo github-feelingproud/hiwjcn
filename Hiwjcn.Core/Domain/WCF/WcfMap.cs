b70a936556ae1ec145686f5cca5f1e62228aaeca
王俊 <hiwjcn@live.com>
2017-09-26 15:03:03 +0800

    8

---
+++ Hiwjcn.Core/Domain/WCF/WcfMap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib.data;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Model.User;
+using System.Runtime.Serialization;
+using Model;
+using System.Configuration;
+using Lib.extension;
+
+namespace Hiwjcn.Core.Domain.WCF
+{
+    [Serializable]
+    [Table("wcf_map")]
+    public class WcfMap : BaseEntity
+    {
+        [Required]
+        public virtual string ContractName { get; set; }
+
+        [Required]
+        public virtual string SvcUrl { get; set; }
+
+        [Required]
+        public virtual DateTime HeartBeatsTime { get; set; }
+    }
+}
--- Hiwjcn.Framework/Tasks/WcfHeartBeatTask.cs
+++ Hiwjcn.Framework/Tasks/WcfHeartBeatTask.cs
@@ -9,7 +9,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Lib.core;
+using Lib.ioc;
+using Lib.data;
 using Polly;
+using Hiwjcn.Core.Domain.WCF;
 
 namespace Hiwjcn.Framework.Tasks
 {
@@ -23,39 +27,107 @@ namespace Hiwjcn.Framework.Tasks
 
         public override ITrigger Trigger => this.TriggerInterval(15);
 
-        public override void Execute(IJobExecutionContext context)
+        private void PushToQueue(ref Queue queue)
         {
-            var queue = new Queue();
-            //add task to queue
-
-            void Worker()
+            using (var s = AppContext.Scope())
             {
+                var repo = s.Resolve_<IRepository<WcfMap>>();
+                var max_iid = 0.00;
                 while (true)
                 {
-                    try
+                    var list = repo.QueryList(where: x => x.IID > max_iid, orderby: x => x.IID, Desc: false, count: 200);
+                    if (!ValidateHelper.IsPlumpList(list))
                     {
-                        var url = string.Empty;
-                        lock (queue)
-                        {
-                            if (queue.Count <= 0) { break; }
-                            url = (string)queue.Dequeue();
-                        }
-                        //request url and update last update time
-
-                        Policy.Handle<Exception>().Retry(3).Execute(() => { });
+                        //生产完成
+                        return;
                     }
-                    catch (Exception e)
+                    foreach (var x in list)
                     {
-                        e.AddErrorLog();
+                        queue.Enqueue(x);
                     }
+                    max_iid = list.Max(x => x.IID);
                 }
             }
+        }
 
-            var thread_list = new List<Thread>(Com.Range(5).Select(x => new Thread(() => Worker()) { IsBackground = true }));
-            //开启消费
-            thread_list.ForEach(t => t.Start());
-            //等待结束
-            thread_list.ForEach(t => t.Join());
+        private void ConsumeQueue(ref Queue queue, Func<bool> stop)
+        {
+            while (!stop.Invoke())
+            {
+                try
+                {
+                    var model = default(WcfMap);
+                    lock (queue)
+                    {
+                        if (queue.Count <= 0)
+                        {
+                            //队列为空，等一会儿
+                            Thread.Sleep(200);
+                            continue;
+                        }
+                        model = (queue.Dequeue() as WcfMap) ?? throw new Exception("queue中拿出的数据为null");
+                    }
+                    //request url and update last update time
+
+                    Policy.Handle<Exception>().WaitAndRetry(3, i => TimeSpan.FromMilliseconds(i * 100)).Execute(() =>
+                    {
+                        HttpClientHelper.Get(model.SvcUrl);
+                        using (var s = AppContext.Scope())
+                        {
+                            var repo = s.Resolve_<IRepository<WcfMap>>();
+                            var m = repo.GetFirst(x => x.IID == model.IID);
+                            m.Update();
+                            m.HeartBeatsTime = m.UpdateTime;
+                            repo.Update(m);
+                        }
+                    });
+                }
+                catch (Exception e)
+                {
+                    e.AddErrorLog();
+                    Thread.Sleep(200);
+                }
+            }
+        }
+
+        public override void Execute(IJobExecutionContext context)
+        {
+            Action<long, string> logger = (ms, name) =>
+            {
+                $"{this.Name},耗时：{ms}毫秒".AddBusinessInfoLog();
+            };
+            using (var timer = new CpuTimeLogger(logger))
+            {
+                var stop = false;
+                var thread_count = 5;
+                var queue = new Queue();
+                //add task to queue
+
+                Thread CreateThread() => new Thread(() => this.ConsumeQueue(ref queue, () => stop)) { IsBackground = true };
+                var tts = Com.Range(thread_count).Select(x => CreateThread());
+                var thread_list = new List<Thread>(tts);
+
+                try
+                {
+                    //开启消费
+                    thread_list.ForEach(t => t.Start());
+
+                    //开启生产
+                    this.PushToQueue(ref queue);
+                }
+                catch (Exception e)
+                {
+                    e.AddErrorLog();
+                }
+                finally
+                {
+                    //生产结束，消费可以结束
+                    stop = true;
+                    //等待结束
+                    thread_list.ForEach_(t => t.Join());
+                    thread_list.Clear();
+                }
+            }
         }
     }
 }
