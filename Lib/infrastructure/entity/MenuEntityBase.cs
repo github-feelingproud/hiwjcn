81f8d048a3ce7792ccc22112ec14e13a49d33d08
王俊 <hiwjcn@live.com>
2017-11-11 14:59:48 +0800

    0

--- Lib/infrastructure/entity/MenuEntityBase.cs
+++ Lib/infrastructure/entity/MenuEntityBase.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lib.mvc.user;
+using Lib.extension;
+using Lib.helper;
 
 namespace Lib.infrastructure.entity
 {
+    /// <summary>
+    /// 导航/边栏/轮播都属于菜单
+    /// </summary>
     public class MenuEntityBase : TreeEntityBase
     {
         public virtual string MenuName { get; set; }
@@ -14,10 +20,51 @@ namespace Lib.infrastructure.entity
 
         public virtual string Url { get; set; }
 
+        public virtual string ImageUrl { get; set; }
+
+        public virtual string IconCls { get; set; }
+
+        public virtual string IconUrl { get; set; }
+
+        public virtual string HtmlId { get; set; }
+
+        public virtual string HtmlCls { get; set; }
+
+        public virtual string HtmlStyle { get; set; }
+
+        public virtual string HtmlOnClick { get; set; }
+
         public virtual int Sort { get; set; }
 
         public virtual string PermissionJson { get; set; }
 
-        public virtual string Group { get; set; }
+        public virtual string GroupKey { get; set; }
+        
+        public virtual bool ShowForUser(LoginUserInfo loginuser)
+        {
+            var pers = this.PermissionJson?.JsonToEntity<List<string>>();
+            if (ValidateHelper.IsPlumpList(pers))
+            {
+                foreach (var p in pers)
+                {
+                    if (!(loginuser?.HasPermission(p) ?? false))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
+    public class MenuGroupEntityBase : BaseEntity
+    {
+        public virtual string GroupKey { get; set; }
+
+        public virtual string GroupName { get; set; }
+
+        public virtual string Description { get; set; }
+
+        public virtual string ImageUrl { get; set; }
     }
 }
--- Lib/infrastructure/service/MenuServiceBase.cs
+++ Lib/infrastructure/service/MenuServiceBase.cs
@@ -14,44 +14,73 @@ using Lib.infrastructure.extension;
 
 namespace Lib.infrastructure.service
 {
-    public interface IMenuServiceBase<MenuBase>
+    public interface IMenuServiceBase<MenuBase, MenuGroupBase>
     {
         Task<_<string>> AddMenu(MenuBase model);
 
-        Task<_<string>> DeleteMenuRecursively(string menu_uid);
+        Task<_<string>> DeleteMenuRecursively(string group_key, string menu_uid);
 
         Task<_<string>> DeleteMenus(params string[] menu_uids);
 
-        Task<List<MenuBase>> QueryMenuList(string group, string parent = null);
+        Task<List<MenuBase>> QueryMenuList(string group_key, string parent = null);
 
         Task<_<string>> UpdateMenu(MenuBase model);
     }
 
-    public abstract class MenuServiceBase<MenuBase> : IMenuServiceBase<MenuBase>
+    public abstract class MenuServiceBase<MenuBase, MenuGroupBase> :
+        IMenuServiceBase<MenuBase, MenuGroupBase>
         where MenuBase : MenuEntityBase, new()
+        where MenuGroupBase : MenuGroupEntityBase, new()
     {
         private readonly IRepository<MenuBase> _menuRepo;
+        private readonly IRepository<MenuGroupBase> _menuGroupRepo;
 
         public MenuServiceBase(
-            IRepository<MenuBase> _menuRepo)
+            IRepository<MenuBase> _menuRepo,
+            IRepository<MenuGroupBase> _menuGroupRepo)
         {
             this._menuRepo = _menuRepo;
+            this._menuGroupRepo = _menuGroupRepo;
         }
 
-        public virtual async Task<_<string>> AddMenu(MenuBase model) =>
-            await this._menuRepo.AddTreeNode(model, "mn");
+        public virtual async Task<_<string>> AddMenu(MenuBase model)
+        {
+            var data = new _<string>();
+            if (!await this._menuGroupRepo.ExistAsync(x => x.GroupKey == model.GroupKey))
+            {
+                data.SetErrorMsg("菜单分组不存在");
+                return data;
+            }
 
-        public virtual async Task<_<string>> DeleteMenuRecursively(string menu_uid) =>
-            await this._menuRepo.DeleteTreeNodeRecursively(menu_uid);
+            return await this._menuRepo.AddTreeNode(model, "mn");
+        }
+
+        public virtual async Task<_<string>> DeleteMenuRecursively(string group_key, string menu_uid)
+        {
+            if (!ValidateHelper.IsPlumpString(group_key)) { throw new ArgumentNullException(nameof(group_key)); }
+            var data = new _<string>();
+            var list = await this._menuRepo.GetListEnsureMaxCountAsync(x => x.GroupKey == group_key, 5000, "树结构节点数量达到上限");
+            var node = list.Where(x => x.UID == menu_uid).FirstOrThrow("节点不存在");
+            var dead_nodes = await list.AsQueryable().FindNodeChildrenRecursively_(node);
+
+            if (await this._menuRepo.DeleteAsync(dead_nodes.ToArray()) > 0)
+            {
+                data.SetSuccessData(string.Empty);
+                return data;
+            }
+
+            throw new Exception("删除错误");
+        }
 
         public virtual async Task<_<string>> DeleteMenus(params string[] menu_uids) =>
             await this._menuRepo.DeleteByMultipleUIDS(menu_uids);
 
-        public virtual async Task<List<MenuBase>> QueryMenuList(string group, string parent = null)
+        public virtual async Task<List<MenuBase>> QueryMenuList(string group_key, string parent = null)
         {
+            if (!ValidateHelper.IsPlumpString(group_key)) { throw new ArgumentNullException(nameof(group_key)); }
             return await this._menuRepo.PrepareIQueryableAsync_(async query =>
             {
-                query = query.Where(x => x.Group == group);
+                query = query.Where(x => x.GroupKey == group_key);
                 if (ValidateHelper.IsPlumpString(parent))
                 {
                     query = query.Where(x => x.ParentUID == parent);
