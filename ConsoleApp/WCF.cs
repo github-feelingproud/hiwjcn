cf68c6b25181aee1cb9d6a0deef2863c7585ba53
王俊 <hiwjcn@live.com>
2017-11-24 16:54:53 +0800

    0

--- ConsoleApp/WCF.cs
+++ ConsoleApp/WCF.cs
@@ -21,15 +21,29 @@ namespace ConsoleApp
         public DateTime Time() => DateTime.Now;
     }
 
+    [ServiceContract]
+    public interface mm
+    {
+        [OperationContract]
+        DateTime Date();
+    }
+
+    public class mmImpl : mm
+    {
+        public DateTime Date() => DateTime.Now;
+    }
+
     public static class WCF
     {
         public static void Serv()
         {
-            using (var host = new ServiceHost(typeof(xxImpl), new Uri("http://localhost:10000/")))
+            var list = new List<ServiceHost>();
+
+            try
             {
-                try
                 {
-                    host.AddServiceEndpoint(typeof(xx), new BasicHttpBinding(), "hello");
+                    var host = new ServiceHost(typeof(xxImpl), new Uri("http://localhost:10000/xx/"));
+                    host.AddServiceEndpoint(typeof(xx), new BasicHttpBinding(), "xx");
 
                     var smb = new ServiceMetadataBehavior();
                     smb.HttpGetEnabled = true;
@@ -37,16 +51,34 @@ namespace ConsoleApp
                     host.Description.Behaviors.Add(smb);
 
                     host.Open();
-                    Console.WriteLine("服务已启动");
-                    Console.ReadKey();
+                    list.Add(host);
                 }
-                catch (Exception e)
+
                 {
-                    Console.WriteLine(e.GetInnerExceptionAsJson());
+                    var host = new ServiceHost(typeof(mmImpl), new Uri("http://localhost:10000/mm/"));
+                    host.AddServiceEndpoint(typeof(mm), new BasicHttpBinding(), "mm");
+
+                    var smb = new ServiceMetadataBehavior();
+                    smb.HttpGetEnabled = true;
+                    smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                    host.Description.Behaviors.Add(smb);
+
+                    host.Open();
+                    list.Add(host);
                 }
-                finally
+                Console.WriteLine("服务已启动");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetInnerExceptionAsJson());
+            }
+            finally
+            {
+                foreach (var s in list)
                 {
-                    host.Close();
+                    s.Close();
+                    ((IDisposable)s).Dispose();
                 }
             }
         }
