79024b1ed691ada64fcb787bf14ab990d6c0e0c3
王俊 <hiwjcn@live.com>
2017-11-27 19:14:17 +0800

    0

---
+++ Lib/infrastructure/helper/TreeHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib.infrastructure.model;
+using Lib.helper;
+using Lib.extension;
+using Lib.infrastructure.extension;
+using Lib.infrastructure.entity;
+
+namespace Lib.infrastructure.helper
+{
+    public static class TreeHelper
+    {
+        /// <summary>
+        /// 把扁平结构变成树形结构
+        /// </summary>
+        public static IEnumerable<ZTreeNode> BuildZTreeStructure(IEnumerable<ZTreeNode> list)
+        {
+            if (list.Any(x => !ValidateHelper.IsPlumpString(x.id))) { throw new Exception("每个节点都需要id"); }
+
+            var data = list.Where(x => !ValidateHelper.IsPlumpString(x.pId));
+            var repeat = new List<string>();
+
+            void BindChildren(ref IEnumerable<ZTreeNode> nodes)
+            {
+                foreach (var m in nodes)
+                {
+                    repeat.AddOnceOrThrow("树存在错误");
+
+                    var children = list.Where(x => x.pId == m.id);
+                    if (ValidateHelper.IsPlumpList(children))
+                    {
+                        BindChildren(ref children);
+
+                        m.children = children.ToList();
+                    }
+                }
+            }
+
+            BindChildren(ref data);
+
+            return data;
+        }
+
+        /// <summary>
+        /// 把扁平结构变成树形结构
+        /// </summary>
+        public static IEnumerable<IViewTreeNode> BuildIViewTreeStructure(IEnumerable<IViewTreeNode> list)
+        {
+            if (list.Any(x => !ValidateHelper.IsPlumpString(x.id))) { throw new Exception("每个节点都需要id"); }
+
+            var data = list.Where(x => !ValidateHelper.IsPlumpString(x.pId));
+            var repeat = new List<string>();
+
+            void BindChildren(ref IEnumerable<IViewTreeNode> nodes)
+            {
+                foreach (var m in nodes)
+                {
+                    repeat.AddOnceOrThrow("树存在错误");
+
+                    var children = list.Where(x => x.pId == m.id);
+                    if (ValidateHelper.IsPlumpList(children))
+                    {
+                        BindChildren(ref children);
+
+                        m.children = children.ToList();
+                    }
+                }
+            }
+
+            BindChildren(ref data);
+
+            return data;
+        }
+    }
+}
---
+++ Lib/infrastructure/model/IViewTreeNode.cs
@@ -0,0 +1,64 @@
+﻿using Lib.infrastructure.entity;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.infrastructure.model
+{
+    /// <summary>
+    /// https://www.iviewui.com/components/tree
+    /// </summary>
+    [Serializable]
+    [JsonObject(MemberSerialization.OptOut)]
+    public class IViewTreeNode
+    {
+        [JsonProperty(Required = Required.Always)]
+        public virtual string id { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public virtual string pId { get; set; }
+
+        public virtual string title { get; set; }
+
+        public virtual bool expand { get; set; } = false;
+
+        public virtual bool disabled { get; set; } = false;
+
+        public virtual bool disableCheckbox { get; set; } = false;
+
+        public virtual bool selected { get; set; } = false;
+
+        public virtual bool @checked { get; set; } = false;
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public virtual List<IViewTreeNode> children { get; set; }
+
+
+        public static implicit operator IViewTreeNode(RoleEntityBase role) =>
+            new IViewTreeNode()
+            {
+                id = role.UID,
+                pId = role.ParentUID,
+                title = role.RoleName,
+            };
+
+        public static implicit operator IViewTreeNode(DepartmentEntityBase department) =>
+            new IViewTreeNode()
+            {
+                id = department.UID,
+                pId = department.ParentUID,
+                title = department.DepartmentName
+            };
+
+        public static implicit operator IViewTreeNode(MenuEntityBase menu) =>
+            new IViewTreeNode()
+            {
+                id = menu.UID,
+                pId = menu.ParentUID,
+                title = menu.MenuName
+            };
+    }
+}
