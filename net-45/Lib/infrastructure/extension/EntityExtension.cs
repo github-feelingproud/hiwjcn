4961a742895069496c2b1c609c4d1ea8338dc60e
王俊 <hiwjcn@live.com>
2017-12-20 17:11:04 +0800

    0

--- net-45/Lib/infrastructure/extension/EntityExtension.cs
+++ net-45/Lib/infrastructure/extension/EntityExtension.cs
@@ -14,6 +14,50 @@ namespace Lib.infrastructure.extension
 {
     public static class EntityExtension
     {
+        public static IQueryable<T> FilterDateRange<T>(this IQueryable<T> query,
+            DateTime? start, DateTime? end,
+            Func<IQueryable<T>, DateTime, IQueryable<T>> start_filter,
+            Func<IQueryable<T>, DateTime, IQueryable<T>> end_filter,
+            bool drop_time = true)
+            where T : BaseEntity
+        {
+            if (start != null)
+            {
+                if (drop_time)
+                {
+                    start = start.Value.Date;
+                }
+                query = start_filter.Invoke(query, start.Value);
+            }
+            if (end != null)
+            {
+                if (drop_time)
+                {
+                    end = end.Value.Date.AddDays(1);
+                }
+                query = end_filter.Invoke(query, end.Value);
+            }
+            return query;
+        }
+
+        public static IQueryable<T> FilterCreateDateRange<T>(this IQueryable<T> query,
+            DateTime? start, DateTime? end, bool drop_time = true)
+            where T : BaseEntity =>
+            query.FilterDateRange(
+                start, end,
+                (q, d) => q.Where(x => x.CreateTime >= d),
+                (q, d) => q.Where(x => x.CreateTime < d),
+                drop_time);
+
+        public static IQueryable<T> FilterUpdateDateRange<T>(this IQueryable<T> query,
+            DateTime? start, DateTime? end, bool drop_time = true)
+            where T : BaseEntity =>
+            query.FilterDateRange(
+                start, end,
+                (q, d) => q.Where(x => x.UpdateTime >= d),
+                (q, d) => q.Where(x => x.UpdateTime < d),
+                drop_time);
+
         public static async Task<_<string>> DeleteByMultipleUIDS_<T>(this IRepository<T> repo, params string[] uids)
             where T : BaseEntity
         {
@@ -31,7 +75,7 @@ namespace Lib.infrastructure.extension
 
             throw new Exception("删除数据错误");
         }
-        
+
         public static async Task<_<string>> AddEntity_<T>(this IRepository<T> repo, T model, string model_flag)
             where T : BaseEntity
         {
