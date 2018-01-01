496558abfa6eab6b634a281a2902c8d61d55e4d2
王俊 <hiwjcn@live.com>
2018-01-01 14:14:34 +0800

    0

--- net-45/Lib/extension/EnumExtension.cs
+++ net-45/Lib/extension/EnumExtension.cs
@@ -9,6 +9,17 @@ namespace Lib.extension
 {
     public static class EnumExtension
     {
+        /// <summary>
+        /// 用来获取枚举成员
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static Dictionary<string, object> GetPublicStaticFieldsValues(this Type t)
+        {
+            var fields = t.GetFields(BindingFlags.Static | BindingFlags.Public);
+            return fields.ToDictionary(x => x.Name, x => x.GetValue(null));
+        }
+
         public static Dictionary<string, T> GetItems<T>(this Enum data)
         {
             //Enum.GetValues();
@@ -18,14 +29,6 @@ namespace Lib.extension
             var keys = fields.Select(x => x.GetValue(data)).ToList();
 
             return fields.ToDictionary(x => x.Name, x => (T)x.GetValue(data));
-        }
-
-        public static Dictionary<string, object> GetItems(this Enum data)
-        {
-            var fields = data.GetType().GetFields(BindingFlags.Static | BindingFlags.Public);
-            var keys = fields.Select(x => x.GetValue(data)).ToList();
-
-            return fields.ToDictionary(x => x.Name, x => x.GetValue(data));
         }
     }
 }
