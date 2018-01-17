7da5f2e8be3536cfc881842ffa06e0b2c3520df4
王俊 <hiwjcn@live.com>
2018-01-17 18:07:32 +0800

    9

--- net-45/Hiwjcn.Service/AuthLoginProvider.cs
+++ net-45/Hiwjcn.Service/AuthLoginProvider.cs
@@ -1,7 +1,10 @@
 ﻿using Hiwjcn.Bll.User;
 using Hiwjcn.Core.Domain.User;
+using Lib.data.ef;
+using Lib.extension;
 using Lib.helper;
 using Lib.infrastructure.service.user;
+using Lib.ioc;
 using Lib.mvc;
 using Lib.mvc.auth;
 using Lib.mvc.user;
@@ -13,6 +16,46 @@ using System.Threading.Tasks;
 
 namespace Hiwjcn.Bll.Auth
 {
+    public interface IUserLoginService :
+        IUserLoginServiceBase<UserEntity>,
+        IAutoRegistered
+    { }
+
+    public class UserLoginService :
+        UserLoginServiceBase<UserEntity, UserOneTimeCodeEntity, RolePermissionEntity, UserRoleEntity, UserDepartmentEntity, DepartmentRoleEntity, PermissionEntity>,
+        IUserLoginService
+    {
+        public UserLoginService(
+            IEFRepository<UserDepartmentEntity> _userDepartmentRepo,
+            IEFRepository<DepartmentRoleEntity> _departmentRoleRepo,
+            IEFRepository<UserEntity> _userRepo,
+            IEFRepository<UserOneTimeCodeEntity> _oneTimeCodeRepo,
+            IEFRepository<RolePermissionEntity> _rolePermissionRepo,
+            IEFRepository<UserRoleEntity> _userRoleRepo,
+            IEFRepository<PermissionEntity> _perRepo) :
+            base(_userDepartmentRepo, _departmentRoleRepo, _userRepo, _oneTimeCodeRepo, _rolePermissionRepo, _userRoleRepo, _perRepo)
+        {
+            //
+        }
+
+        public override string EncryptPassword(string password)
+        {
+            return password.Trim().ToMD5().Trim().ToUpper();
+        }
+
+        public override async Task<_<string>> RegisterCheck(UserEntity model)
+        {
+            var data = new _<string>();
+            if (await this._userRepo.ExistAsync(x => x.UserName == model.UserName))
+            {
+                data.SetErrorMsg("用户名已存在");
+                return data;
+            }
+            data.SetSuccessData(string.Empty);
+            return data;
+        }
+    }
+
     public class AuthLoginProvider : IAuthLoginProvider
     {
         private readonly IUserLoginService _login;
@@ -40,6 +83,7 @@ namespace Hiwjcn.Bll.Auth
                 Email = model.Email,
 
                 Permissions = model.PermissionNames ?? new List<string>() { },
+                Roles = model.RoleIds ?? new List<string>() { },
                 IsActive = model.IsActive,
             };
 
